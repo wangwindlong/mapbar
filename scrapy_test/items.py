156c8c2893f8f3e0e5439ec0b903f3f31666242a
wangyl-mac-x <yunlong782@126.com>
2019-09-06 18:36:02 +0800

    添加其他界面的爬取

--- scrapy_test/items.py
+++ scrapy_test/items.py
@@ -9,38 +9,31 @@ import scrapy
 
 
 class ScrapyTestItem(scrapy.Item):
-    # define the fields for your item here like:
-    name = scrapy.Field()
-    url = scrapy.Field()
+    name = scrapy.Field()  # 城市的名字
+    url = scrapy.Field()  # 城市的链接地址
 
 
-class AddressTypeItem(scrapy.Item):
-    # define the fields for your item here like:
-    cityname = scrapy.Field()
-    cityurl = scrapy.Field()
+class AddressTypeItem(ScrapyTestItem):
+    # def __init__(self, length, breadth, unit_cost=0, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.length = length
+    #     self.breadth = breadth
+    #     self.unit_cost = unit_cost
 
-    typename = scrapy.Field()
-    typetitle = scrapy.Field()
-    typeurl = scrapy.Field()
+    typename = scrapy.Field()  # 地址的类别名称
+    typetitle = scrapy.Field()  # 地址的类别名称
+    typeurl = scrapy.Field()  # 地址的类别url
 
 
-class AddressItem(scrapy.Item):
-    # define the fields for your item here like:
+class AddressItem(AddressTypeItem):
     province = scrapy.Field()  # 省
     city = scrapy.Field()  # 市
     county = scrapy.Field()  # 县
     town = scrapy.Field()  # 镇
     address = scrapy.Field()  # 详细地址
 
-    cityurl = scrapy.Field()  # 城市的链接地址
-    cityname = scrapy.Field()  # 城市的名字
-    typetitle = scrapy.Field()  # 地址的类别名称
-    typeurl = scrapy.Field()  # 地址的类别url
     addressname = scrapy.Field()  # 小区的名字
     addressurl = scrapy.Field()  # 小区的详细地址url
 
     location = scrapy.Field()  # WGS84经纬度 需要转bd09坐标
     updatetime = scrapy.Field()  # 地址更新日期
-
-
-
--- scrapy_test/spiders/example.py
+++ scrapy_test/spiders/example.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-from scrapy_test.items import ScrapyTestItem
+from scrapy_test.items import ScrapyTestItem, AddressTypeItem, AddressItem
 from scrapy.selector import Selector
 
 
@@ -31,57 +31,87 @@ class ExampleSpider(scrapy.Spider):
 
         ul_path = ulselector.xpath('./li[contains(., "地址")]')
         address_path = ul_path.xpath('text()').getall()
+        addr = address_path[-1].strip()
         if len(address_path) > 0:
-            print("li address =", address_path[-1].strip())
+            print("li address =", addr)
 
         town = ''
-        address = ulselector.xpath("./li[contains(., '地址')]/a")
-        for item in address:
-            item_str = item.xpath("text()").get().strip()
+        addresses = ulselector.xpath("./li[contains(., '地址')]/a")
+        for address in addresses:
+            item_str = address.xpath("text()").get().strip()
             if msg['city'] != item_str:
                 town = item_str
             print("item =", item_str)
 
         province = msg['province']
         city = msg['city']
-        print("address =", province, city, town, address_path[-1].strip())
+        item = response.meta['item']
+        print("address =", province, city, town, addr)
+        addressItem = AddressItem(province=province, city=city, county=town, address=addr,
+                                  addressurl=item.get('addressurl'), addressname=item.get('addressname'),
+                                  typeurl=item.get('typeurl'), typename=item.get('typename'),
+                                  name=item.get('name'), url=item.get('url'))
+        yield addressItem
 
     def getaddresses(self, response):
         dl_selectors = Selector(response=response).xpath('//div[@class="sortC"]/dl')
         print("dl_selectors ", dl_selectors)
+        item = response.meta['item']
         for dl_selector in dl_selectors:
             print("dl_selector ", dl_selector)
             a_selectors = dl_selector.xpath('./dd/a[@href]')
             for selector in a_selectors:
-                item = ScrapyTestItem()  # 实例化一个 DmozItem 类
-                item['name'] = selector.xpath("text()").get()
-                item['url'] = selector.xpath("@href").get()
-                print("selector item =", item)
+                addressname = selector.xpath("text()").get()
+                addressurl = selector.xpath("@href").get()
+                addressItem = AddressItem(addressurl=addressurl, addressname=addressname,
+                                          typeurl=item.get('typeurl'), typename=item.get('typename'),
+                                          name=item.get('name'), url=item.get('url'))
+                print("getaddresses addressItem =", addressItem)
+                request = response.follow(addressurl, callback=self.getaddressdetail)
+                request.meta['from'] = response.url
+                request.meta['item'] = addressItem
+                yield request
                 # yield self.getaddresstype(item)
                 # yield item
 
-    def gettypes(self, selectors):
-        print("selectors ", selectors)
-        a_selectors = selectors.xpath('./a[@href]')
-        print("a_selectors ", a_selectors)
-        for selector in a_selectors:
-            typeurl = selector.xpath('@href').get()
-            type = selector.xpath('text()').get()
-            print("typeurl=", typeurl, "type=", type)
+    # def gettypes(self, selectors, response):
 
     def getaddresstype(self, response):
         row_selectors = Selector(response=response).xpath('//div[@class="isortRow"]')
         # for row_selector in row_selectors:
         if len(row_selectors) > 1:
-            row_selector = row_selectors[-1]
+            row_selector = row_selectors[-1].xpath('./h3')
             print("row_selector ", row_selector)
-            # 只获取小区数据
-            house_selectors = row_selector.xpath('./h3[contains(., "小区")]/following-sibling::div[1]')
-            business_selectors = row_selector.xpath('./h3[contains(., "商务")]/following-sibling::div[1]')
-            self.gettypes(house_selectors)
-            self.gettypes(business_selectors)
+            for row in row_selector:
+                if "小区" not in row.get() and "商务" not in row.get():
+                    continue
+                # yield self.gettypes(, response)
+                selectors = row.xpath('following-sibling::div[1]')
+                item = response.meta['item']
+                print("selectors ", selectors)
+                a_selectors = selectors.xpath('./a[@href]')
+                print("a_selectors ", a_selectors)
+                for selector in a_selectors:
+                    typeurl = selector.xpath('@href').get()
+                    type = selector.xpath('text()').get()
+                    typeItem = AddressTypeItem(typeurl=typeurl, typename=type, name=item.get('name'),
+                                               url=item.get('url'))
+                    print("gettypes typeItem=", typeItem)
+                    request = response.follow(typeurl, callback=self.getaddresses)
+                    request.meta['from'] = response.url
+                    request.meta['item'] = typeItem
+                    yield request
 
-    def getcity(self, response):
+            # # 只获取小区数据
+            # house_selectors = row_selector.xpath('./h3[contains(., "小区")]/following-sibling::div[1]')
+            # business_selectors = row_selector.xpath('./h3[contains(., "商务")]/following-sibling::div[1]')
+            # self.gettypes(house_selectors)
+            # self.gettypes(business_selectors)
+
+    # def getcity(self, response):
+
+    def parse(self, response):
+        print("parse url:", response)
         dd_selectors = Selector(response=response).xpath('//dl[contains(@class, "city_list")]/dd')
         for dd_selector in dd_selectors:
             print("dd_selector ", dd_selector)
@@ -91,10 +121,9 @@ class ExampleSpider(scrapy.Spider):
                 item['name'] = selector.xpath("text()").get()
                 item['url'] = selector.xpath("@href").get()
                 print("selector item =", item)
-                yield self.getaddresstype(item)
-
-    def parse(self, response):
-        print("parse url:", response)
-        self.getcity(response)
+                request = response.follow(item['url'], callback=self.getaddresstype)
+                request.meta['from'] = response.url
+                request.meta['item'] = item
+                yield request
 
 # json.dumps(d, ensure_ascii=False, encoding='utf-8'))
